ca78caa4ee518f41840fc32679ce23172efb0eb3
Cam T. Bui <ncbui@users.noreply.github.com>
2020-07-10 22:05:38 -0700

    clarified comments and removed tester variables

--- connect4.js
+++ connect4.js
@@ -9,7 +9,7 @@
 
 
 class Game {
-  //constructor height, width, maybe board
+  /** initialize all instances with these properties */
     constructor(height = 6, width = 7) {
     this.height = height;
     this.width = width;
@@ -18,15 +18,15 @@ class Game {
     this.makeArrayBoard();
   }
 
-  /** Board constructed within the constructor methods */
-  /** makeBoard: create in-JS board structure called arrayBoard:
-  *   arrayBoard = array of rows, each row is array of cells  (arrayBoard[y][x])
+  /** 
+  Board constructed within the constructor methods 
+  makeArrayBoard: create in-JS board structure called arrayBoard:
+  contains array of rows, each row is array of cells  (arrayBoard[y][x])
   */
   makeArrayBoard() {
     
     this.arrayBoard = [];
     for (let y = 0; y < this.height; y++) {
-      // need to debug syntax
       this.arrayBoard.push(Array.from({ length: this.width }))
     }
   }
@@ -35,35 +35,29 @@ class Game {
   makeHtmlBoard() {
     //it is posssible to interact with Dom w/in the class =QUESTION!!
     const htmlBoard = document.getElementById('board');
-    // this.board = document.getElementById('board');
 
     // make column tops (clickable area for adding a piece to that column)
     const top = document.createElement('tr');
     let handleClickInstance = this;
-    // let thisHandleClick = handleClick.bind(this)
-    // this.top = document.createElement('tr');
     top.setAttribute('id', 'column-top');
     top.addEventListener('click', this.handleClick.bind(handleClickInstance));
 
     for (let x = 0; x < this.width; x++) {
       const headCell = document.createElement('td');
-      // this.headCell = document.createElement('td');
       headCell.setAttribute('id', x);
       top.append(headCell);
     }
 
     htmlBoard.append(top);
-    // board.append(top);
 
     // make main part of board
     for (let y = 0; y < this.height; y++) {
       const row = document.createElement('tr');
-      // this.row = document.createElement('tr');
 
       for (let x = 0; x < this.width; x++) {
         const cell = document.createElement('td');
-        // this.cell = document.createElement('td');
         //add this to cell and & row below
+        // give each cell an id according to its location
         cell.setAttribute('id', `${y}-${x}`);
         row.append(cell);
       }
@@ -75,7 +69,6 @@ class Game {
   /** findSpotForCol: given column x, return top empty y (null if filled) */
   findSpotForCol(x) {
     for (let y = this.height - 1; y >= 0; y--) {
-      //this.board[y][x]
       if (!this.arrayBoard[y][x]) {
         return y;
       }
@@ -116,25 +109,23 @@ class Game {
     this.placeInTable(y, x);
     // console.log("arrayBoard has updated at", y,x, this.arrayBoard)
 
-    // TODO: Add timeout
+    // Delay alert so that piece is placed in HTMLBoard
     // check for win
     if (this.checkForWin()) {
-      // return this.endGame(`Player ${this.currPlayer} won!`);
       setTimeout(() => {
         this.endGame(`Player ${this.currPlayer} won!`);
       }, 10);
     }
 
-    // TODO: Add timeout
+    // Delay alert so that piece is placed in HTMLBoard
     // check for tie
     if (this.arrayBoard.every(row => row.every(cell => cell))) {
-      // return this.endGame('Tie!');
       setTimeout(() => {
         this.endGame('Tie!');
       }, 10);
     }
 
-    // TODO: Add timeout
+    // Delay player switch until checkForWin() && tie is run
     // switch players
     setTimeout(() => {
       this.currPlayer = this.currPlayer === 1 ? 2 : 1;
@@ -156,9 +147,8 @@ class Game {
         this.arrayBoard[y][x] === this.currPlayer
     );
   }
-  //Question: 
-  // best practice for creating helper fuction to be a difference method within
-  // the same class
+  //Question: best practice for creating helper fuction within a method
+
   /** checkForWin: check arrayBoard cell-by-cell for "does a win start here?" */
   checkForWin() {
     // console.log("within checkforwin(), this is: ", this, this.arrayBoard)
